2c1f7630a21104c7be920cde603ad2f90c19fc1d
vokoshyv <ronsuij@gmail.com>
2015-08-24 22:32:15 -0700

    adds server for using node to mine data

---
+++ apiMatchData/miningServer.js
@@ -0,0 +1,11 @@
+var apiKey = require('../APIKEY/apiKey');
+
+console.log(apiKey['key']);
+
+// var request = require('request');
+
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage.
+//   }
+// })
