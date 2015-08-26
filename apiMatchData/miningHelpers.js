32ff82aee604ddfd48c05219cca494a64bf1c00b
vokoshyv <ronsuij@gmail.com>
2015-08-26 13:26:47 -0700

    setting up some of the architecture for miningHelers.js

--- apiMatchData/miningHelpers.js
+++ apiMatchData/miningHelpers.js
@@ -1,1 +1,14 @@
 var fs = require('fs');
+var request = require('request');
+
+
+
+var queryForMatches = function(){
+  request('http://www.google.com', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+    }
+  })
+}
+
+module.exports = queryForMatches;
