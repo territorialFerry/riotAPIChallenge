d4e26db2d19dc4041debecd7da11678e4bb9d148
vokoshyv <ronsuij@gmail.com>
2015-08-29 14:49:42 -0700

    makes miningServer.js code more modular for different region data

--- apiMatchData/miningServer.js
+++ apiMatchData/miningServer.js
@@ -1,5 +1,11 @@
 var queryForMatches = require('./miningHelpers');
+var rankedBeforePatchNA = require('../AP_ITEM_DATASET/5.11/RANKED_SOLO/NA');
+var rankedAfterPatchNA = require('../AP_ITEM_DATASET/5.14/RANKED_SOLO/NA');
+var rankedBeforePatchEUW = require('../AP_ITEM_DATASET/5.11/RANKED_SOLO/EUW');
+var rankedAfterPatchEUW = require('../AP_ITEM_DATASET/5.14/RANKED_SOLO/EUW');
+var rankedBeforePatchKR = require('../AP_ITEM_DATASET/5.11/RANKED_SOLO/KR');
+var rankedAfterPatchKR = require('../AP_ITEM_DATASET/5.14/RANKED_SOLO/KR');
 
-queryForMatches('na', 'BEFORE', 'test');
+queryForMatches('euw', 'BEFORE', 'test', rankedBeforePatchEUW);
 
 
