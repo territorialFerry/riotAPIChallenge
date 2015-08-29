0b1bbcfa73e113d6cef57d8e134e8ad10dd58e6a
vokoshyv <ronsuij@gmail.com>
2015-08-29 16:41:05 -0700

    importing database for use in miningServer.js

--- apiMatchData/miningServer.js
+++ apiMatchData/miningServer.js
@@ -1,3 +1,4 @@
+// match ID importation
 var queryForMatches = require('./miningHelpers');
 var rankedBeforePatchNA = require('../AP_ITEM_DATASET/5.11/RANKED_SOLO/NA');
 var rankedAfterPatchNA = require('../AP_ITEM_DATASET/5.14/RANKED_SOLO/NA');
@@ -5,6 +6,8 @@ var rankedBeforePatchEUW = require('../AP_ITEM_DATASET/5.11/RANKED_SOLO/EUW');
 var rankedAfterPatchEUW = require('../AP_ITEM_DATASET/5.14/RANKED_SOLO/EUW');
 var rankedBeforePatchKR = require('../AP_ITEM_DATASET/5.11/RANKED_SOLO/KR');
 var rankedAfterPatchKR = require('../AP_ITEM_DATASET/5.14/RANKED_SOLO/KR');
+
+// bring in the mysql database
 
 queryForMatches('euw', 'BEFORE', 'test', rankedBeforePatchEUW);
 
