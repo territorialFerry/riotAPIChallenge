f5c84f0ac94ee2a7204dcc855dd8acb5b70d62dd
vokoshyv <ronsuij@gmail.com>
2015-08-29 20:52:23 -0700

    code works here for insertion of champion data into TEST table in database

--- apiMatchData/championData/championInitialInsertion.js
+++ apiMatchData/championData/championInitialInsertion.js
@@ -1,10 +1,20 @@
 var database = require('../database/db');
 var champData = require('./championID');
 
-console.log(champData);
+var champions = champData['data'];
 
-database.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
- 
-  console.log('The solution is: ', rows[0].solution);
-});
+// for loop to go through elements of champions
+var queryString;
+for (var champ in champions){
+  queryString = 'INSERT INTO TEST (CHAMP_ID, TITLE) values (' + champions[champ]['id'] + ', "' + champions[champ]['title'] + '");'
+  console.log("THE STRING", queryString);
+  
+  // insertion into the database
+  database.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+   
+    console.log('The test data has been inserted');
+  });
+  
+}
+
